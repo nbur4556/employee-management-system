13569f2bd22f2604afe0e6514e253ffdff1c7379
nbur4556 <nbur4556@gmail.com>
2020-10-08 07:38:28 -0500

    read function available on DbConnection class

--- database-connection.js
+++ database-connection.js
@@ -19,9 +19,45 @@ class DbConnection {
             console.log(`Successfully connected to MySQL database`);
         });
     }
+
+    // Create data on the database
+    create(tableName, colNames) {
+
+    }
+
+    // Read data on the database
+    read(tableName, query = '*') {
+        return new Promise((resolve, reject) => {
+            this.connection.query(`SELECT ${query} FROM ${tableName}`, (err, res) => {
+                if (err) reject(err);
+
+                resolve(res);
+                this.connection.end();
+            });
+        });
+    }
+
+    // Update data on the database
+    update() {
+
+    }
+
+    // Delete data on the database
+    delete() {
+
+    }
 }
 
 const employeeManagementConnection = new DbConnection('employee_management_db');
-employeeManagementConnection.testConnection();
+// employeeManagementConnection.testConnection();
+
+// READ TEST
+employeeManagementConnection.read('fake')
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 module.exports = DbConnection;
